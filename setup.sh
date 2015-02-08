9aae090233d9f52b4ac3a9dbfc55f89d9c7781cb
Steven Heidel <steven@heidel.ca>
2015-02-07 18:31:33 -0800

    Updated resume and removed old files

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 #bin/sh
-apt-get install texlive-common unzip texlive-binaries texlive-latex-recommended texlive-latex-extra
+sudo apt-get install texlive-full unzip texlive-binaries texlive-latex-recommended texlive-latex-extra
 
-mkdir -p /usr/local/share/texmf/tex/latex/moderncv
+sudo mkdir -p /usr/local/share/texmf/tex/latex/moderncv
 
 wget https://launchpad.net/moderncv/trunk/1.5.1/+download/moderncv-1.5.1.zip
-unzip moderncv-1.5.1.zip 
-rm moderncv-1.5.1.zip 
+unzip moderncv-1.5.1.zip
+rm moderncv-1.5.1.zip
 
 cd moderncv
-
-cp *.sty *.cls -t /usr/local/share/texmf/tex/latex/moderncv/
+sudo cp *.sty *.cls -t /usr/local/share/texmf/tex/latex/moderncv/
 cd .. && rm -rf moderncv
-mktexlsr
+
+sudo mktexlsr
